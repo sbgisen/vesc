d1b6a3c6b7c129bf222efb0c8ec121a5753a82b7
jsupratman13 <supratmanjoshua@gmail.com>
2022-12-21 18:59:55 +0900

    wait until calibration finished

--- vesc_hw_interface/src/vesc_servo_controller.cpp
+++ vesc_hw_interface/src/vesc_servo_controller.cpp
@@ -319,17 +319,19 @@ void VescServoController::updateSensor(const std::shared_ptr<VescPacket const>& 
     velocity_sens_ = velocity_rpm / 60.0 * 2.0 * M_PI * gear_ratio_;                                 // unit: rad/s
     effort_sens_ = current * torque_const_ / gear_ratio_;
 
-    if (joint_type_ == "revolute")
-    {
-      position_sens_ = angles::normalize_angle(position_sens_ * 2 * M_PI);  // convert to radians
-    }
-    else if (joint_type_ == "continuous")
-    {
-      position_sens_ = position_sens_ * 2 * M_PI;  // convert to radians
-    }
-    else if (joint_type_ == "prismatic")
-    {
-      position_sens_ = position_sens_ * screw_lead_;
+    if (!calibration_flag_){
+      if (joint_type_ == "revolute")
+      {
+        position_sens_ = angles::normalize_angle(position_sens_ * 2 * M_PI);  // convert to radians
+      }
+      else if (joint_type_ == "continuous")
+      {
+        position_sens_ = position_sens_ * 2 * M_PI;  // convert to radians
+      }
+      else if (joint_type_ == "prismatic")
+      {
+        position_sens_ = position_sens_ * screw_lead_;
+      }
     }
   }
   return;

9c5afb2285fa33e69964a2c02048d467cc143775
saitenntaisei <saitenntaisei@gmail.com>
2025-02-14 11:32:40 +0900

    format

--- vesc_hw_interface/launch/position_control_sample.launch.py
+++ vesc_hw_interface/launch/position_control_sample.launch.py
@@ -31,27 +31,32 @@ from launch_ros.substitutions import FindPackageShare
 
 def launch_setup(context: LaunchContext, *args, **kwargs) -> list:
 
-    vesc_pkg = FindPackageShare('vesc_hw_interface').find('vesc_hw_interface')
-    doc = xacro.process_file(LaunchConfiguration('model').perform(context))
-    robot_description = {'robot_description': doc.toprettyxml(indent='  ')}
+    vesc_pkg = FindPackageShare("vesc_hw_interface").find("vesc_hw_interface")
+    doc = xacro.process_file(LaunchConfiguration("model").perform(context))
+    robot_description = {"robot_description": doc.toprettyxml(indent="  ")}
 
-    robot_controllers = [vesc_pkg, '/config/position_sample.yaml']
+    robot_controllers = [vesc_pkg, "/config/position_sample.yaml"]
 
-    control_node = Node(package='controller_manager',
-                        executable='ros2_control_node',
-                        parameters=[robot_description, robot_controllers],
-                        output='both')
+    control_node = Node(
+        package="controller_manager", executable="ros2_control_node", parameters=[robot_description, robot_controllers], output="both"
+    )
 
-    controllers = GroupAction(actions=[
-        Node(package='controller_manager',
-             executable='spawner',
-             output='both',
-             arguments=['--controller-manager', 'controller_manager', 'joint_state_broadcaster']),
-        Node(package='controller_manager',
-             executable='spawner',
-             output='both',
-             arguments=['--controller-manager', 'controller_manager', 'joint_position_controller'])
-    ])
+    controllers = GroupAction(
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                output="both",
+                arguments=["--controller-manager", "controller_manager", "joint_state_broadcaster"],
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                output="both",
+                arguments=["--controller-manager", "controller_manager", "joint_position_controller"],
+            ),
+        ]
+    )
 
     return [control_node, controllers]
 
@@ -62,7 +67,7 @@ def generate_launch_description() -> LaunchDescription:
     Returns:
         Launch descriptions
     """
-    vesc_pkg = pathlib.Path(FindPackageShare('vesc_hw_interface').find('vesc_hw_interface'))
-    model_arg = DeclareLaunchArgument('model', default_value=str(vesc_pkg / 'launch/position_test.ros2_control.xacro'))
+    vesc_pkg = pathlib.Path(FindPackageShare("vesc_hw_interface").find("vesc_hw_interface"))
+    model_arg = DeclareLaunchArgument("model", default_value=str(vesc_pkg / "launch/position_test.ros2_control.xacro"))
 
     return LaunchDescription([model_arg, OpaqueFunction(function=launch_setup)])
